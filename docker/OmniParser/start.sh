3568363d17c689ffeda2be3f139b7bcf78c9d820
jcgarcia <julio.jcgc@gmail.com>
2025-02-21 10:05:50 -0500

    change repository name

--- docker/OmniParser/start.sh
+++ docker/OmniParser/start.sh
@@ -3,5 +3,5 @@ docker run -it --rm --gpus '"device=4,5"' \
     --name OmniParser \
     --net="host" \
     -v $(dirname $(dirname $PWD)):/OmniParser \
-    jcgarciaca/OmniParser:latest \
+    milleinnovacion/omniparser:latest \
     /bin/bash
